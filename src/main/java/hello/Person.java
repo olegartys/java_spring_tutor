4ec9f2b5a7c6e542645658e965dfee9f35713c1c
Oleg Levin <oleg.levin@yotadevices.com>
2017-05-25 11:22:44 +0300

    second

--- src/main/java/hello/Person.java
+++ src/main/java/hello/Person.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class zPerson {
+public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
