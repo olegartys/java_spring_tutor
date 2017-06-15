07e47bb0f01e9ee5740cc42b7d6bd88a94f0c8f6
Oleg Levin <oleg.levin@yotadevices.com>
2017-06-15 08:55:25 +0300

    sync

--- init.sh
+++
@@ -1,3 +0,0 @@
-curl --data "username=user&password=password" http://localhost:8080/login --cookie-jar cookies.txt
-curl -i -X POST -H "Content-Type:application/json" -d "{  \"firstName\" : \"$1\",  \"lastName\" : \"$2\" }" http://localhost:8080/people --cookie cookies.txt
-curl http://localhost:8080/people --cookie cookies.txt
